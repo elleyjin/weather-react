d75549f4feaed1adda2a0ef6f70f06e31674fa98
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-19 22:40:16 +0800

    added axios & loader package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from "./logo.svg";
 import Weather from "./Weather";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather />
+        <Weather city="New York" />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,33 @@
 import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
-export default function Weather() {
-  return <h1>Hello</h1>;
+export default function Weather(props) {
+  let apiKey = "094780c710fa4efd669f0df8c3991927";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  function displayWeather(response) {
+    alert(
+      `The temperature in ${props.city} is ${Math.round(
+        response.data.main.temp
+      )}`
+    );
+  }
+
+  axios.get(apiUrl).then(displayWeather);
+
+  return (
+    <div>
+      <h2>Hello</h2>
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="white"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+    </div>
+  );
 }
